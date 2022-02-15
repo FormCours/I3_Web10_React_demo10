da680417b8c7e39372dd8ad149a1629cad4b3d86
Bastien Gorissen <kadomony@gmail.com>
2022-02-15 15:04:47 +0100

    Add List Message

--- src/containers/message-container/message-container.jsx
+++ src/containers/message-container/message-container.jsx
@@ -1,11 +1,31 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
 import MessageForm from "../../components/message-form/message-form";
+import MessageList from "../../components/message-list/message-list";
 
 const MessageContainer = () => {
+
+    const [messages, setMessages] = useState([]);
+
+    const handleNewMessage = (msg) => {
+        // Création d'un objet avec le message et un id unique
+        const newMsg = {
+            msg: msg,
+            id: nanoid()
+        };
+
+        // Ajout du message en 1er element
+        setMessages(messages => [newMsg, ...messages]);
+        
+        // ↓ Alternative : Même chose avec la methode "concat"
+        //setMessages(messages => [].concat(newMsg).concat(messages));
+    }
 
     return (
         <main>
             <h1>Message...</h1>
-            <MessageForm />
+            <MessageForm onSendMsg={handleNewMessage} />
+            <MessageList incomingMessages={messages} />
         </main>
     )
 }
