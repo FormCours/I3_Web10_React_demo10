e469f633884a9daaa1ab1b5d828eb799802792af
Bastien Gorissen <kadomony@gmail.com>
2022-02-15 12:20:56 +0100

    Add message (Partial)

---
+++ src/containers/message-container/message-container.jsx
@@ -0,0 +1,13 @@
+import MessageForm from "../../components/message-form/message-form";
+
+const MessageContainer = () => {
+
+    return (
+        <main>
+            <h1>Message...</h1>
+            <MessageForm />
+        </main>
+    )
+}
+
+export default MessageContainer;
